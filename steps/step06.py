41ae36810c5e6ac369944abd81a33c285989b6df
Soh1121 <satou.shg@gmail.com>
2020-09-24 16:23:27 +0900

    [update]バックプロパゲーションの実装
    実際に計算してみる

--- steps/step06.py
+++ steps/step06.py
@@ -43,27 +43,17 @@ class Exp(Function):
         return gx
 
 
-def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data) / (2 * eps)
-
-
-f = Square()
-x = Variable(np.array(2.0))
-dy = numerical_diff(f, x)
-print(dy)
-
-
-def f(x):
-    A = Square()
-    B = Exp()
-    C = Square()
-    return C(B(A(x)))
-
+A = Square()
+B = Exp()
+C = Square()
 
 x = Variable(np.array(0.5))
-dy = numerical_diff(f, x)
-print(dy)
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+b.grad = C.backward(y.grad)
+a.grad = B.backward(b.grad)
+x.grad = A.backward(a.grad)
+print(x.grad)
