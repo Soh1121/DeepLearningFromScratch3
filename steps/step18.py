601201f6e56582c4e7e7ae5a0e273e069b82307d
Soh1121 <satou.shg@gmail.com>
2020-10-07 11:47:07 +0900

    [update]with文による切り替えを実装

--- steps/step18.py
+++ steps/step18.py
@@ -1,10 +1,25 @@
 import unittest
 import weakref
 import numpy as np
+import contextlib
 
 
 class Config:
     enable_backprop = True
+
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+
+def no_grad():
+    return using_config('enable_backprop', False)
 
 
 class Variable:
@@ -134,3 +149,11 @@ y.backward()
 Config.enable_backprop = False
 x = Variable(np.ones((100, 100, 100)))
 y = square(square(square(x)))
+
+with using_config('enable_backprop', False):
+    x = Variable(np.array(2.0))
+    y = square(x)
+
+with no_grad():
+    x = Variable(np.array(2.0))
+    y = square(x)
