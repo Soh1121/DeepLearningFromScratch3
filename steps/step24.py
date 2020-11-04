0194f48e9a345c639c4c21bfb5ad712b41a79bf0
Soh1121 <satou.shg@gmail.com>
2020-11-04 16:10:06 +0900

    [update]matyas関数を実装

--- steps/step24.py
+++ steps/step24.py
@@ -12,8 +12,19 @@ def sphere(x, y):
     return z
 
 
+def matyas(x, y):
+    z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
+    return z
+
+
 x = Variable(np.array(1.0))
 y = Variable(np.array(1.0))
 z = sphere(x, y)
+z.backward()
+print(x.grad, y.grad)
+
+x = Variable(np.array(1.0))
+y = Variable(np.array(1.0))
+z = matyas(x, y)
 z.backward()
 print(x.grad, y.grad)
