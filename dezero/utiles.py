7ac329c2d91aab9ff0012d7ea5d245c3dd324b22
Soh1121 <satou.shg@gmail.com>
2020-11-09 18:55:34 +0900

    [update]DeZeroの関数をDOT言語に変換する関数を実装

--- dezero/utiles.py
+++ dezero/utiles.py
@@ -8,3 +8,15 @@ def _dot_var(v, verbose=False):
         name += str(v.shape) + ' ' + str(v.dtype)
 
     return dot_var.format(id(v), name)
+
+
+def _dot_func(f):
+    dot_func = '{} [label="{}", color=lightblue, style=filled, shape=box]\n'
+    ret = dot_func.format(id(f), f.__class__.__name__)
+
+    dot_edge = '{} -> {}\n'
+    for x in f.inputs:
+        ret += dot_edge.format(id(x), id(f))
+    for y in f.outputs:  # y is weakref
+        ret += dot_edge.format(id(f), id(y()))
+    return ret
