ab8dd3f9813b4468c157edce8195a74866b43544
Soh1121 <satou.shg@gmail.com>
2020-11-17 10:50:51 +0900

    [updateaxisとkeepdimsを実装

--- dezero/functions.py
+++ dezero/functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from dezero import utils
 from dezero.core import Function, as_variable
 
 
@@ -90,12 +91,18 @@ def transpose(x):
 # sum / sum_to / broadcast_to / average / matmul / linear
 # =============================================================================
 class Sum(Function):
+    def __init__(self, axis, keepdims):
+        self.axis = axis
+        self.keepdims = keepdims
+
     def forward(self, x):
         self.x_shape = x.shape
-        y = x.sum()
+        y = x.sum(axis=self.axis, keepdims=self.keepdims)
         return y
 
     def backward(self, gy):
+        gy = utils.reshape_sum_backward(gy, self.x_shape, self.axis,
+                                        self.keepdims)
         gx = broadcast_to(gy, self.x_shape)
         return gx
 
